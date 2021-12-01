d0ce57eb436ebf416b9060866de6aec4b8acca71
unknown <naiuhz@gmail.com>
2021-11-30 20:22:53 -0500

    Added login page

---
+++ src/main.js
@@ -0,0 +1,63 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const usernameForm = document.querySelector("#username");
+    const passwordForm = document.querySelector("#password");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+        console.log("B"); 
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+        console.log("C"); 
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+        console.log("A"); 
+        // Perform your AJAX/Fetch login
+        let username = usernameForm.value; //usernameForm.value;
+        let password = passwordForm.value;
+        setFormMessage(loginForm, "error", username + ":" + password);
+        //setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    //TODO: event listener for sign up
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+            //console.log("B"); 
+        });
+    });
+});
